c119be4c2232a9f3c3d16e98ec542c549da1c449
Akuntahu22 <91618909+Akuntahu22@users.noreply.github.com>
2021-11-02 15:21:21 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,30 @@
+import os
+from os import getenv
+
+from dotenv import load_dotenv
+
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
+
+que = {}
+SESSION_NAME = getenv("SESSION_NAME", "BQAXeq46obeHQL6Sz_iJwbMAk_jnT4Ytbg5hfCZnRmCjZHP_fXb6iP9EiHULcJhYmfSpF70gOCOjz1_C1h1-AEOcdb3PabzVR4Xow5gtRGefxQ1kay3dzWnIIAhVlzR5I_-cQoqO-RzPh8aESfdz4jHyXxTO1pEFR6bMPTvweTRmpIOFR156D8XIdiZBc0st-4dZYn2B9WVxWSDs9M1FQdRM1elQ59Vp0qO4sP8Kheq3m523oY0aS7odrkpnUvXLg8YraY0q7yGDe7F8p5BYUNx_akw_SRJyoFZ5MenlaMHR-aBw_FnB-AoBMWMKCSy4vmnBbvYiCGCzpJf35LK0vPmvaT9o4gA")
+BOT_TOKEN = getenv("BOT_TOKEN", "2072190749:AAHz8P1_ADtIp4n9p4UdRvS1pyGxldjnD4Q")
+BOT_NAME = getenv("BOT_NAME", "Alexa")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "-1001319845035")
+BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/36256d986b9c69902e4c4.png")
+admins = {}
+API_ID = int(getenv("API_ID", "6847114"))
+API_HASH = getenv("API_HASH", "7f2c9b9ac20e6840d13f9ca85e1c4e2d")
+BOT_USERNAME = getenv("BOT_USERNAME", "alexagroup_bot")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "alexaassisten")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "robotprojectx")
+PROJECT_NAME = getenv("PROJECT_NAME", "Alexa")
+OWNER = getenv("OWNER", "justthetech")
+SOURCE_CODE = getenv("SOURCE_CODE", "github.com/Vckyou/Geez-MusicProject")
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "15"))
+ARQ_API_KEY = getenv("ARQ_API_KEY", None)
+PMPERMIT = getenv("PMPERMIT", None)
+LOG_GRP = getenv("LOG_GRP", None)
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
+
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "927625147").split()))
---
+++ handlers/private.py
@@ -0,0 +1,42 @@
+# Daisyxmusic (Telegram bot project )
+# Copyright (C) 2021  Inukaasith
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import logging
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from config import ASSISTANT_NAME,PROJECT_NAME,SUPPORT_GROUP,UPDATES_CHANNEL,BOT_USERNAME
+logging.basicConfig(level=logging.INFO)
+
+@Client.on_message(
+    filters.command("reload")
+    & filters.group
+    & ~ filters.edited
+)
+async def reload(client: Client, message: Message):
+    await message.reply_text("""✅ Bot **berhasil dimulai ulang!**\n\n• **Daftar admin** telah **diperbarui**""",
+      reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+
+                        "☕ ᴜᴘᴅᴀᴛᴇ ☕", url=f"https://t.me/robotprojectx"
+
+                    )
+                ]
+            ]
+        )
+   )
+
